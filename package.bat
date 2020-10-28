b43c409876ee1c8719b00af63d1766ecb475d404
jdah <20308439+jdah@users.noreply.github.com>
2020-10-28 14:34:28 +0100

    Fixed windows build

--- package.bat
+++ package.bat
@@ -1,6 +1,7 @@
 mkdir out\production\Microcraft
 mkdir out\artifacts\Microcraft_jar
 javac -sourcepath src src\com\jdh\microcraft\Main.java -d out\production\Microcraft
-xcopy /s /e res out\production\
+mkdir out\artifacts\Microcraft\res
+xcopy /s /e res out\production\Microcraft\res
 jar -cmvf src\META-INF\MANIFEST.MF out\artifacts\Microcraft_jar\Microcraft.jar -C out\production\Microcraft\ .
 jpackage --name MICROCRAFT --input out\artifacts\Microcraft_jar\ --main-jar Microcraft.jar --main-class com.jdh.microcraft.Main --resource-dir res --icon res\icon.ico
